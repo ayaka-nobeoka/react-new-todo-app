219ae9659d8c63067eb32f88589536a59617e591
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-04-29 11:26:42 +0900

    - [✔️] Vite + React プロジェクトを作成する - [✔️ ] TODO を追加できるフォームを作る - [ ✔️ ] TODO リストを画面に表示するまで完了！！！

--- react-new-todo-app/src/App.jsx
+++ react-new-todo-app/src/App.jsx
@@ -1,12 +1,43 @@
-// import { useState } from 'react'
+import { useState } from "react";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  // const [inout, setInput] = useState("");
-
+  const [input, setInput] = useState("");
+  const [add, setAdd] = useState([]);
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+  const handleAdd = () => {
+    const newTodo = {
+      id: uuidv4(), // ← ここでユニークIDを作る！
+      text: input,
+    };
+    const newArray = [...add, newTodo];
+    if (input.trim() === "") return;
+    setAdd(newArray);
+    setInput("");
+  };
+  const handleDelete = (id) => {
+    const newArray = add.filter((item) => item.id !== id);
+    setAdd(newArray);
+    console.log(newArray);
+  };
+  const handleCompleted = () => {};
   return (
     <>
       <h1>TODO APP</h1>
+      <input value={input} onChange={handleInput} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {add.map((item, id) => (
+          <li key={id}>
+            {item.text}
+            <button onClick={() => handleDelete(item.id)}>削除</button>
+            <button onClick={handleCompleted}>完了</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
